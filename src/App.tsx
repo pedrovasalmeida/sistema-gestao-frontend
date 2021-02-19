9a4bc1f4c34db23eb1e85f98b8db0122d2e3d071
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-19 11:57:48 -0300

    adding toast notifications and auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,26 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 /** Components */
+import { AuthProvider } from './context/AuthContext';
 import Routes from './routes';
 
 /** Global Styles */
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastNotifyContainer } from './styles';
+
+import ThemeProvider from './styles/Theme';
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider>
       <GlobalStyles />
-
-      <BrowserRouter>
+      <AuthProvider>
         <Routes />
-      </BrowserRouter>
-    </>
+      </AuthProvider>
+
+      <ToastNotifyContainer />
+    </ThemeProvider>
   );
 };
 
