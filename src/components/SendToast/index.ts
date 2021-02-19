e90f28fe409cfdd7f496e6b6ea794471efe6fe13
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-19 11:46:16 -0300

    creating send toast component

---
+++ src/components/SendToast/index.ts
@@ -0,0 +1,16 @@
+import { toast } from 'react-toastify';
+
+const SendToast = (message: string, type: 'error' | 'success' | 'info') => {
+  toast(message, {
+    position: 'top-right',
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    className: type,
+  });
+};
+
+export default SendToast;
