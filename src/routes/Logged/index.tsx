bbd3ea372778c5205d1e09078394065e7197e651
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-19 11:56:18 -0300

    redirect from '/' to '/dashboard'

--- src/routes/Logged/index.tsx
+++ src/routes/Logged/index.tsx
@@ -1,6 +1,6 @@
 /** Packages */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 /** Pages */
 import CleanPage from '../../pages/CleanPage';
@@ -8,7 +8,9 @@ import NotFound from '../../pages/NotFound';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={CleanPage} />
+    <Redirect from="/" to="/dashboard" exact />
+
+    <Route path="/dashboard" component={CleanPage} />
     <Route path="/" component={NotFound} />
   </Switch>
 );
