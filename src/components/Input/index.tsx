84e3d9284c4ea6a417716ecbdba27a559d3312b2
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-19 11:46:00 -0300

    creating Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, {
+  InputHTMLAttributes,
+  useRef,
+  useEffect,
+  useState,
+  useCallback,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
+
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const InputComponent: React.FC<InputProps> = ({
+  icon: Icon,
+  name,
+  ...rest
+}) => {
+  const { fieldName, registerField } = useField(name);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [inputError, setInputError] = useState(false);
+
+  const verifyInputField = useCallback(() => {
+    if (fieldName === 'email') {
+      const searchAt = inputRef.current?.value.includes('@');
+      const searchDot = inputRef.current?.value.includes('.');
+
+      if (!searchDot || !searchAt) {
+        setInputError(true);
+      } else {
+        setInputError(false);
+      }
+    }
+
+    if (fieldName === 'password' && inputRef.current) {
+      if (inputRef.current?.value.length < 4) {
+        setInputError(true);
+      } else {
+        setInputError(false);
+      }
+    }
+  }, [fieldName]);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+
+    verifyInputField();
+  }, [verifyInputField]);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container
+      inputError={inputError}
+      isFilled={isFilled}
+      isFocused={isFocused}
+    >
+      {Icon && <Icon size={25} />}
+      <input
+        onFocus={() => setIsFocused(true)}
+        onBlur={handleInputBlur}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default InputComponent;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,69 @@
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  inputError: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  border: 2px solid #ddd;
+  border-radius: 12px;
+  height: 50px;
+
+  margin-bottom: 30px;
+
+  svg {
+    position: absolute;
+    left: 15px;
+    top: 12px;
+
+    color: ${p => p.theme.colors.darkBlue};
+  }
+
+  ${p =>
+    p.isFocused &&
+    css`
+      border: 2px solid ${shade(0.2, p.theme.colors.lightBlue)};
+
+      svg {
+        color: ${shade(0.2, p.theme.colors.green)};
+      }
+    `}
+  ${p =>
+    p.isFilled &&
+    css`
+      svg {
+        color: ${shade(0.2, p.theme.colors.green)};
+      }
+    `}
+  ${p =>
+    p.inputError &&
+    css`
+      border: 2px solid ${p.theme.colors.red};
+
+      svg {
+        color: ${p.theme.colors.red};
+      }
+    `}
+
+  input {
+    width: 100%;
+
+    margin-left: 60px;
+    margin-right: 20px;
+
+    background: #eee;
+    border: 0;
+
+    color: ${p => p.theme.colors.darkBlue};
+    font-weight: 700;
+  }
+`;
+
+export const Input = styled.input``;
