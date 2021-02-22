3ff4df73973493ee0f268050d3fef9e92a1e8a1a
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-22 19:10:18 -0300

    fix

--- src/pages/CleanPage/styles.ts
+++ src/pages/CleanPage/styles.ts
@@ -2,14 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  max-width: 100vw;
-  height: 100vh;
-
-  padding: 50px;
+  height: calc(100vh - 56px);
+  width: 100%;
 
   background-color: ${p => p.theme.colors.background};
 `;
